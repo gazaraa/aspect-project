3efa243ffa96b05e7869fa1d8d5e27e93a5a9f61
Ali Hesham <18P7355@eng.asu.edu.eg.com>
2023-12-18 16:40:35 +0200

    created frontend for our application

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,6 +6,7 @@ import "./navbar.css";
 export const Navbar = () => {
   return (
     <div className="navbar">
+      <h1 className="title">Ain Shams Store</h1>
       <div className="links">
         <Link to="/"> Shop </Link>
         <Link to="/contact"> Contact </Link>
