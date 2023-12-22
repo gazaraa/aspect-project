d0144c4e514da7bccd033c1d8564daa423a3249e
gazara <alihesham2000@gmail.com>
2023-12-22 23:34:45 +0200

    created frontend and backend directories

---
+++ ainshams-store-backend/src/test/java/com/project/ainshamsstorebackend/AinshamsStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ainshamsstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AinshamsStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
