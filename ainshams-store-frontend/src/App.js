2f11bc4f425f09975bf2c93f020d1cc45fdf26f2
Ali Hesham <18P7355@eng.asu.edu.eg.com>
2023-12-23 17:50:23 +0200

    added a login page

--- ainshams-store-frontend/src/App.js
+++ ainshams-store-frontend/src/App.js
@@ -5,17 +5,36 @@ import { Shop } from "./pages/shop/shop";
 import { Contact } from "./pages/contact";
 import { Cart } from "./pages/cart/cart";
 import { ShopContextProvider } from "./context/shop-context";
-
+import { useEffect, useState } from "react";
+import Home from "./pages/home";
+import Login from "./pages/login";
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
   return (
     <div className="App">
       <ShopContextProvider>
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Shop />} />
+            <Route
+              path="/"
+              element={
+                <Home
+                  email={email}
+                  loggedIn={loggedIn}
+                  setLoggedIn={setLoggedIn}
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
+            />
+
             <Route path="/contact" element={<Contact />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/shop" element={<Shop />} />
           </Routes>
         </Router>
       </ShopContextProvider>
